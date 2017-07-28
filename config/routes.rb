01596b84edc1db37f986305867289c70fc4b417f
nisalikularatne <nisalikularatne@yahoo.com>
2017-07-28 11:49:19 +0400

    new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'categories/index'
 
   get 'categories/edit'
@@ -8,8 +10,9 @@ Rails.application.routes.draw do
   get 'categories/show'
 
   root 'home#index'
-  resources:posts
-  resources:categories
+  resources :posts
+  resources :categories
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
